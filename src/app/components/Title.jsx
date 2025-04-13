de483af6230136b6b7a2819a2ab3ce75d8021f49
Alexis <acruz113@alumnos.uaq.mx>
2025-04-13 15:54:31 -0600

    fCommit
    13-04-25

---
+++ src/app/components/Title.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Title( {text} ) { // 
+  return (
+    <div>
+        <p className="font-serif text-7xl">
+            {text}
+        </p>
+    </div>
+  )
+}
+
+export default Title
